b146ed3cbe237b3f280bd93abcd87e5ba5a92964
Kapposszta <szabi0620@gmail.com>
2024-12-10 00:02:50 +0100

    Update game_management_pkg.sql

--- game_management_pkg.sql
+++ game_management_pkg.sql
@@ -1,15 +1,15 @@
 CREATE OR REPLACE PACKAGE game_management_package AS
-    PROCEDURE add_map(p_map_name VARCHAR2, p_path_data VARCHAR2, p_gold_reward NUMBER, p_xp_reward NUMBER);
+    PROCEDURE add_map(p_map_name VARCHAR2, p_path_data VARCHAR2, p_gold_reward NUMBER, p_xp_reward NUMBER, p_wood_reward NUMBER, p_brick_reward NUMBER);
     PROCEDURE play_match(p_user_id NUMBER, p_map_id NUMBER, p_result VARCHAR2);
     PROCEDURE delete_result(p_result_id NUMBER);
 END game_management_package;
 CREATE OR REPLACE PACKAGE BODY game_management_package AS
 
     -- Pálya hozzáadása
-    PROCEDURE add_map(p_map_name VARCHAR2, p_path_data VARCHAR2, p_gold_reward NUMBER, p_xp_reward NUMBER) IS
+    PROCEDURE add_map(p_map_name VARCHAR2, p_path_data VARCHAR2, p_gold_reward NUMBER, p_xp_reward NUMBER, p_wood_reward NUMBER, p_brick_reward NUMBER) IS
     BEGIN
-        INSERT INTO Map_table (map_name, path_data, gold_reward, xp_reward)
-        VALUES (p_map_name, p_path_data, p_gold_reward, p_xp_reward);
+        INSERT INTO Map_table (map_name, path_data, gold_reward, xp_reward, Wood_Reward, Brick_Reward)
+        VALUES (p_map_name, p_path_data, p_gold_reward, p_xp_reward, p_wood_reward, p_brick_reward);
     END;
 
     -- Meccs lejátszása
